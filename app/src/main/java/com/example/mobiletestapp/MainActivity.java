56c1d5a8b8628b767512c9ee39a8247112e91a10
Sevo98 <sevo98.sk@gmail.com>
2023-02-05 20:32:08 +0500

    Создал загрузочный экран с лого

---
+++ app/src/main/java/com/example/mobiletestapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mobiletestapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5*1000);
+    }
+}

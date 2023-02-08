65a0fe697a008c01cd9008c6956ac4cea5f91462
Kirill Sevostyanov <sev@d-element.ru>
2023-02-08 18:15:58 +0500

    Написал rest запрос к адресу с получением данных

--- app/src/main/java/com/example/mobiletestapp/HomeActivity.java
+++ app/src/main/java/com/example/mobiletestapp/HomeActivity.java
@@ -2,12 +2,46 @@ package com.example.mobiletestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class HomeActivity extends AppCompatActivity {
+
+    String URL = "https://d-element.ru/test_api.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response Error", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
     }
 }
--- app/src/main/java/com/example/mobiletestapp/MainActivity.java
+++ app/src/main/java/com/example/mobiletestapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.mobiletestapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

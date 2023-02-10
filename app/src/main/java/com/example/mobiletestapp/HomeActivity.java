dce845ddcf3e93f910854a2057e0a2ecbf97b67a
Kirill Sevostyanov <sev@d-element.ru>
2023-02-10 13:41:28 +0500

    Вытащил данные из json в форму приложения

--- app/src/main/java/com/example/mobiletestapp/HomeActivity.java
+++ app/src/main/java/com/example/mobiletestapp/HomeActivity.java
@@ -3,45 +3,78 @@ package com.example.mobiletestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
     String URL = "https://d-element.ru/test_api.php";
+
+    TextView idTextView, nameTextView, priceTextView, articleTextView;
+
+    String name, price, article;
+
+    JSONArray jsonStorage = new JSONArray();
+
+    ArrayList<JSONObject> elements = new ArrayList<JSONObject>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        nameTextView = findViewById(R.id.name);
+        priceTextView = findViewById(R.id.price);
+        articleTextView = findViewById(R.id.article);
+
         RequestQueue requestQueue = Volley.newRequestQueue(this);
+
 
         JsonObjectRequest objectRequest = new JsonObjectRequest(
                 Request.Method.GET,
                 URL,
                 null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.e("Rest Response", response.toString());
+                response -> {
+                    Log.e("Rest Response", response.toString());
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("items");
+                        setJsonArray(jsonArray);
+
+                        for (int i = 0; i < jsonStorage.length(); i++) {
+                            elements.add(i, jsonStorage.getJSONObject(i));
+                        }
+                        name = (String) elements.get(0).get("name");
+                        price = (String) elements.get(0).get("price").toString();
+                        article = (String) elements.get(0).get("article").toString();
+
+                        setValues();
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
                     }
                 },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Rest Response Error", error.toString());
-                    }
-                }
+                error -> Log.e("Rest Response Error", error.toString())
         );
 
         requestQueue.add(objectRequest);
+    }
+
+    private void setJsonArray(JSONArray jsonArray) {
+        jsonStorage = jsonArray;
+    }
+
+    private void setValues() {
+        nameTextView.setText(name);
+        priceTextView.setText(price);
+        articleTextView.setText(article);
     }
 }
